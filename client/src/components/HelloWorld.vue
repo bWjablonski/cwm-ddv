ea90b1ec55fcf171733046fca0fd51e5e242e9e3
bjablonski <bjablonski@fg-it.eu>
2022-07-17 18:38:54 +0200

    vue + vuetify as client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data: () => ({}),
+  }
+</script>
